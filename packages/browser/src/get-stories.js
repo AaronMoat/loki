a35228a451d0fafa988393d30ae829a80d73478f
Aaron Moat <2937187+AaronMoat@users.noreply.github.com>
2025-05-30 14:30:00 +1000

    Try to support both

--- packages/browser/src/get-stories.js
+++ packages/browser/src/get-stories.js
@@ -4,7 +4,11 @@ const getStories = async (window) => {
   const getStorybook =
     (window.__STORYBOOK_CLIENT_API__ && window.__STORYBOOK_CLIENT_API__.raw) ||
     (window.__STORYBOOK_PREVIEW__ &&
-      window.__STORYBOOK_PREVIEW__.extract && window.__STORYBOOK_PREVIEW__.ready) ||
+      window.__STORYBOOK_PREVIEW__.extract &&
+      window.__STORYBOOK_PREVIEW__.storyStore.raw) ||
+    (window.__STORYBOOK_PREVIEW__ &&
+      window.__STORYBOOK_PREVIEW__.extract &&
+      window.__STORYBOOK_PREVIEW__.ready) ||
     (window.loki && window.loki.getStorybook);
   if (!getStorybook) {
     throw new Error(
@@ -30,11 +34,41 @@ const getStories = async (window) => {
     }
   };
 
-  if (window.__STORYBOOK_PREVIEW__ && window.__STORYBOOK_PREVIEW__.extract) {
-    await window.__STORYBOOK_PREVIEW__.ready();
-    const stories = await window.__STORYBOOK_PREVIEW__.extract();
+  if (
+    window.__STORYBOOK_PREVIEW__ &&
+    window.__STORYBOOK_PREVIEW__.extract &&
+    window.__STORYBOOK_PREVIEW__.ready
+  ) {
+    await window.__STORYBOOK_PREVIEW__.extract();
 
-    return Object.values(stories)
+    // Deprecated, will be removed in V9
+    const stories = window.__STORYBOOK_PREVIEW__.storyStore.raw();
+
+    return stories
+      .map((component) => ({
+        id: component.id,
+        kind: component.kind,
+        story: component.story,
+        parameters: Object.fromEntries(
+          Object.entries(component.parameters || {}).filter(
+            ([key, value]) =>
+              !key.startsWith('__') &&
+              !blockedParams.includes(key) &&
+              isSerializable(value)
+          )
+        ),
+      }))
+      .filter(({ parameters }) => !parameters.loki || !parameters.loki.skip);
+  }
+
+  if (window.__STORYBOOK_PREVIEW__ && window.__STORYBOOK_PREVIEW__.extract) {
+    // New official API to extract stories from preview
+    await window.__STORYBOOK_PREVIEW__.extract();
+
+    // Deprecated, will be removed in V9
+    const stories = window.__STORYBOOK_PREVIEW__.storyStore.raw();
+
+    return stories
       .map((component) => ({
         id: component.id,
         kind: component.kind,
